f3110f304d25b69f8e848758709727185b0f80bd
Stanlyv <SoftPillow1337>
2023-03-28 03:24:13 +0300

    + added gravity, jumps, enemy

--- main.py
+++ main.py
@@ -5,25 +5,61 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font("font/pixelType.ttf", 50)
+text_font = pygame.font.Font("font/pixelType.ttf", 50)
+text_color = (64, 64, 64)
+box_color = "#c0e8ec"
 
+sky_surf = pygame.image.load("graphics/platformerGraphicsDeluxePackOld/sky.png").convert()
+sky_surf = pygame.transform.scale(sky_surf, (800, 300))
+ground_surf = pygame.image.load("graphics/platformerGraphicsDeluxePackOld/groundAndGrass.png").convert()
+ground_surf = pygame.transform.scale(ground_surf, (800, 150))
 
-sky_surface = pygame.image.load("graphics/platformerGraphicsDeluxePackOld/sky.png")
-sky_surface = pygame.transform.scale(sky_surface, (800, 300))
+score_surf = text_font.render("My game", False, text_color)
+score_rect = score_surf.get_rect(center=(400, 50))
 
-ground_surface = pygame.image.load("graphics/platformerGraphicsDeluxePackOld/groundAndGrass.png")
-ground_surface = pygame.transform.scale(ground_surface, (800,150))
+snail_surf = pygame.image.load("graphics/platformerGraphicsDeluxePack/Enemies/snailWalk1.png").convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom=(800, 300))
 
-text_surface = test_font.render("My game", False, "Black")
+player_surf = pygame.image.load("graphics/platformerGraphicsDeluxePack/Player/p1_walk/PNG/p1_walk09.png").convert_alpha()
+player_rect = player_surf.get_rect(midbottom=(80, 300))
+player_gravity = 0
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, 300))
+    pygame.draw.rect(screen, box_color, score_rect)
+    pygame.draw.rect(screen, box_color, score_rect, 10)
+    screen.blit(score_surf, score_rect)
+
+    pygame.draw.ellipse(screen, "Brown", pygame.Rect(50, 200, 100, 100))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 300:
+        player_rect.bottom = 300
+
+    if snail_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
+
+    screen.blit(player_surf, player_rect)
+
     pygame.display.update()
     clock.tick(60)
